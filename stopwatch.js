edaf0c09e7d4bfb33ffd2ddbc8785560bddfd0fd
jnf <jeremy.flores@gmail.com>
2017-06-18 22:01:26 -0700

    adds a couple helper functions to stopwatch.js so we spend time working on callbacks and not string formatting

---
+++ stopwatch.js
@@ -0,0 +1,19 @@
+// turns the time into a human readable format
+function formatTime (raw) {
+  let seconds = Math.floor(raw / 1000)
+  let fractionalSeconds = (raw % 1000) / 1000
+  let minutes = Math.floor(seconds / 60)
+  seconds = seconds - (60 * minutes) + fractionalSeconds
+
+  return `${zeroPad(minutes)}:${zeroPad(seconds.toFixed(2))}`
+}
+
+// adds a leading zero because humans like them
+function zeroPad (value) {
+  let pad = value < 10 ? '0' : ''
+  return `${pad}${value}`
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log('ready!')
+})
